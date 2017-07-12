1958ebd7ee113340616f7cfbbb9b72bedc40e64e
Aaron Caines <aaroncaines@aarons-imac>
2017-07-12 02:19:07 +0100

    Initial Commit

---
+++ DoodleBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoodleBug
+//
+//  Created by Aaron Caines on 12/07/2017.
+//  Copyright © 2017 Aaron Caines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
